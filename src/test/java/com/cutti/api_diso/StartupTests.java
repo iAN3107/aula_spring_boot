7cfb73ebffcf0c48724c3dbfaf1d32e7cf6fde35
Vinícius Ian <vini.ian@hotmail.com>
2023-01-04 13:43:58 -0300

    Initial commit

---
+++ src/test/java/com/cutti/api_diso/StartupTests.java
@@ -0,0 +1,13 @@
+package com.cutti.api_diso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
